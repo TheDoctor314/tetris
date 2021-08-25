6480d32ef9d192a54d2dffead55866ab75a8c154
TheDoctor314 <darsh.rajan95@gmail.com>
2021-08-25 21:16:25 +0530

    Move `ColourMap` to the header and make it inline
    NOTE: Maybe rename ColourMap?
    
    Fixed a release build error.

--- src/tetromino.cpp
+++ src/tetromino.cpp
@@ -1,15 +1,5 @@
 #include "tetromino.hpp"
 
-static const std::array<sf::Color, 7> ColourMap = {
-    sf::Color::Cyan,        // I
-    sf::Color::Blue,        // J
-    sf::Color(255, 165, 0), // L
-    sf::Color::Yellow,      // O
-    sf::Color::Green,       // S
-    sf::Color::Magenta,     // T
-    sf::Color::Red,         // Z
-};
-
 namespace tetris {
 std::array<sf::Vector2i, 4> get_points(TetriType type, sf::Vector2i top_left) {
     using sf::Vector2i;
@@ -190,9 +180,5 @@ void Tetromino::draw(sf::RenderWindow &window, sf::Shape &shape) const {
         shape.setPosition(x, y);
         window.draw(shape);
     }
-}
-
-inline sf::Color get_colour(const TetriType type) {
-    return ColourMap[static_cast<size_t>(type)];
 }
 } // namespace tetris
--- src/tetromino.hpp
+++ src/tetromino.hpp
@@ -9,7 +9,19 @@
 namespace tetris {
 enum class TetriType : uint8_t { I, J, L, O, S, T, Z };
 
-sf::Color get_colour(const TetriType type);
+inline const std::array<sf::Color, 7> ColourMap = {
+    sf::Color::Cyan,        // I
+    sf::Color::Blue,        // J
+    sf::Color(255, 165, 0), // L
+    sf::Color::Yellow,      // O
+    sf::Color::Green,       // S
+    sf::Color::Magenta,     // T
+    sf::Color::Red,         // Z
+};
+
+inline sf::Color get_colour(const TetriType type) {
+    return ColourMap[static_cast<size_t>(type)];
+}
 
 std::array<sf::Vector2i, 4> get_points(TetriType type, sf::Vector2i top_left);
 
